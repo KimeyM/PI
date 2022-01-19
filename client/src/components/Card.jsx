5266b44eeec9b0a525bedbc047b439c6d036c09e
KimeyM <kimey.-@hotmail.com>
2022-01-18 22:29:22 -0300

    finished

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import './Card.css'
-
-// export default function Card ({ flag, name, continent }) {
-//     return (
-//         <div className='cardcountry'>
-//             <div className='imag'>
-//                 <img src={flag} alt="img not found" width="200px" height="150px" />
-//             </div>
-//             <div className='text'>
-//                 {name} <br/>
-//                 ‣ {continent}
-//             </div>
-//         </div>
-//     );
-// };
-
 import React from 'react';
 import './Card.css'
 
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -11,18 +11,9 @@ export default function SearchBar (){
     };
 
     return (
-        // <div className='both'>
-        //     {/* <div className='inputcont'> */}
-        //     <div className="search__container">
-        //         <input type='text' placeholder='🔍 Search by name' className="search__input" onChange={handleSubmit}/>
-
-        //         {/* <input type='text' placeholder='🔍 Search by name' className='input' onChange={handleSubmit}/> */}
-        //     </div>
-        // </div>
         <div class="flexbox">
             <div class="search">
-                {/* <h1>Search countries</h1>
-                <h3>Click on search icon, then type your keyword.</h3> */}
+
                 <div>
                     <input type="text" onChange={handleSubmit} placeholder="🔍 Search..." required  />
                 </div>
